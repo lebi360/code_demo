b4536f67d94ed685521984a06d4c5310ad596998
635941307@qq.com <635941307@qq.com>
2017-02-12 09:46:59 +0800

    implement producer consumer by thread.

---
+++ Thread_Producer_Consumer/src/Mantou.java
@@ -0,0 +1,15 @@
+/**
+ * Created by caihx on 2017/2/11.
+ */ //
+class Mantou
+{
+    private int id;
+
+    Mantou(int id){
+        this.id = id;
+    }
+
+    public String toString(){
+        return "Mantou :" + id;
+    }
+}
